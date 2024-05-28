41734ca4abe7d09c72a7ad0d83cbe3e99013f41e
Enobraed <morzh20031@gmail.com>
2024-05-28 13:06:22 +0300

    First laptop version of the settings

---
+++ Laptop/apps-install.nix
@@ -0,0 +1,31 @@
+{ config, pkgs, ... }:
+
+{
+  environment.systemPackages = with pkgs; [
+    pkgs.audacious
+    pkgs.discord
+    pkgs.betterdiscordctl
+    pkgs.fastfetch
+    pkgs.libreoffice
+    pkgs.lutris
+    pkgs.obs-studio
+    pkgs.parsec-bin
+    pkgs.qbittorrent
+    pkgs.steam
+    pkgs.telegram-desktop
+    pkgs.vlc
+    pkgs.vscodium
+    pkgs.mangohud
+    pkgs.goverlay
+    pkgs.gamescope
+    pkgs.anydesk
+    pkgs.appeditor
+    #GNOME
+    pkgs.gnome.gnome-tweaks
+    pkgs.gnomeExtensions.dash-to-dock
+    pkgs.gnomeExtensions.appindicator
+    pkgs.gnomeExtensions.quick-lang-switch
+    #Icons
+    pkgs.papirus-icon-theme
+  ];
+}
