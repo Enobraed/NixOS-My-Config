d53eec801ee699b17403747a5af26a3df607a5fa
Enobraed <morzh20031@gmail.com>
2024-05-30 16:09:16 +0300

    Обновил приложения

--- Laptop/Apps/install.nix
+++ Laptop/Apps/install.nix
@@ -3,38 +3,38 @@
 {
 	environment.systemPackages = with pkgs; [
 		# Opening files
-		pkgs.audacious
-		pkgs.libreoffice
-		pkgs.vlc
+		audacious
+		libreoffice
+		vlc
 
 		# Communication
-		pkgs.betterdiscordctl
-		pkgs.discord
-		pkgs.telegram-desktop
+		betterdiscordctl
+		discord
+		telegram-desktop
 
 		# Gaming
-		pkgs.gamescope
-		pkgs.lutris
-		pkgs.steam
+		gamescope
+		lutris
+		steam
 
 		# System monitoring
-		pkgs.goverlay
-		pkgs.mangohud
+		goverlay
+		mangohud
 
 		# Development
-		pkgs.git
-		pkgs.vscodium
+		git
+		vscodium
 
 		# Remote access
-		pkgs.anydesk
-		pkgs.parsec-bin
+		anydesk
+		parsec-bin
 
 		# Etc
-		pkgs.appeditor
-		pkgs.fastfetch
-		pkgs.gparted
-		pkgs.libsForQt5.ark
-		pkgs.obs-studio
-		pkgs.qbittorrent
+		appeditor
+		fastfetch
+		gparted
+		libsForQt5.ark
+		obs-studio
+		qbittorrent
 	];
 }
