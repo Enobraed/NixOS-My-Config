ed6de12b80ff2537774116f96ba722ef5d09fc82
Enobraed <morzh20031@gmail.com>
2024-05-30 18:24:48 +0300

    Доработка

--- Laptop/Devices/Bluetooth.nix
+++ Laptop/Devices/Bluetooth.nix
@@ -1,19 +1,17 @@
-{ config, pkgs, ... }:
-
 {
-  # Включить поддержку Bluetooth
-  hardware.bluetooth.enable = true;
+	# Включить поддержку Bluetooth
+	hardware.bluetooth.enable = true;
 
-  # Включение Bluetooth при запуске
-  hardware.bluetooth.powerOnBoot = true;
+	# Включение Bluetooth при запуске
+	hardware.bluetooth.powerOnBoot = true;
 
-  # Включить PipeWire для Bluetooth
-  services.pipewire.wireplumber.extraConfig = {
-    "monitor.bluez.properties" = {
-      "bluez5.enable-sbc-xq" = true;
-      "bluez5.enable-msbc" = true;
-      "bluez5.enable-hw-volume" = true;
-      "bluez5.roles" = [ "hsp_hs" "hsp_ag" "hfp_hf" "hfp_ag" ];
-    };
-  };
+	# Включить PipeWire для Bluetooth
+	services.pipewire.wireplumber.extraConfig = {
+		"monitor.bluez.properties" = {
+			"bluez5.enable-sbc-xq" = true;
+			"bluez5.enable-msbc" = true;
+			"bluez5.enable-hw-volume" = true;
+			"bluez5.roles" = [ "hsp_hs" "hsp_ag" "hfp_hf" "hfp_ag" ];
+		};
+	};
 }
